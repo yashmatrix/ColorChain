a952676c4bd970d76ea8bbfbd7fbf83ea9ae724a
yashmatrix <techhammar@gmail.com>
2026-04-29 09:40:29 +0530

    Fix import path for config.js in app.js

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,6 @@
 import { saveHighScore, getPlayerHighScore, getLeaderboard } from './database.js';
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-import { config } from './config.js';
+import { config } from '/ColorChain/config.js';
 
 let userSeq = [], gameSeq = [], scoreSeq = [];
 let started = false, level = 0;
